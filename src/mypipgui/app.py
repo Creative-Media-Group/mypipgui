b61f154cfa35e9f6f4fdc009f71473b3cec9f767
tct123 <42028373+tct123@users.noreply.github.com>
2024-08-11 02:41:27 +0200

    Update app.py

--- src/mypipgui/app.py
+++ src/mypipgui/app.py
@@ -12,25 +12,36 @@ class MyPipGUI(toga.App):
         lang = locale.getlocale()[0]
         self.file = f"{self.paths.app.absolute()}/resources/localisation.csv"
         main_box = toga.Box()
-        module_input = toga.TextInput(
+        self.module_input = toga.TextInput(
             placeholder=tr(
                 csv_file=self.file, target_key="SEARCHPACKAGE", langcode=lang
-            )
+            ),
+            on_change=self.change_module_input,
+            style=Pack(padding=10, flex=1),
         )
-        searchbtn = toga.Button(
-            text=tr(csv_file=self.file, target_key="SEARCHPACKAGE", langcode=lang)
+        self.searchbtn = toga.Button(
+            text=tr(csv_file=self.file, target_key="SEARCHPACKAGE", langcode=lang),
+            enabled=False,
+            style=Pack(padding=10, flex=1),
         )
-        installbtn = toga.Button(
+        self.installbtn = toga.Button(
             tr(csv_file=self.file, target_key="INSTALLPACKAGE", langcode=lang),
             enabled=False,
+            style=Pack(padding=10, flex=1),
         )
-        main_box.add(module_input)
-        main_box.add(searchbtn)
-        main_box.add(installbtn)
+        main_box.add(self.module_input)
+        main_box.add(self.searchbtn)
+        main_box.add(self.installbtn)
         main_box.style.direction = "column"
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def change_module_input(self, widget):
+        if self.module_input.value != "":
+            self.searchbtn.enabled = True
+        else:
+            self.searchbtn.enabled = False
 
 
 def install(package, version=""):
